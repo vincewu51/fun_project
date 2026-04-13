fa6cc8e4c08d2a45c7652fac813a7fec596f6bd4
sl628 <76009660+sl628@users.noreply.github.com>
2026-04-12 20:44:08 -0700

    update eval command

--- aic_notes/record.sh
+++ aic_notes/record.sh
@@ -17,3 +17,16 @@ pixi run lerobot-record \
   ### remove old cache
   rm -rf ~/.cache/huggingface/lerobot/siyulw2025/ca
   ble-insertion-0407
+
+Gazebo sim evaluation
+# Indicate distrobox to use Docker as container manager
+export DBX_CONTAINER_MANAGER=docker
+
+# Create and enter the eval container
+docker pull ghcr.io/intrinsic-dev/aic/aic_eval:latest
+# If you do *not* have an NVIDIA GPU, remove the --nvidia flag for GPU support
+distrobox create -r --nvidia -i ghcr.io/intrinsic-dev/aic/aic_eval:latest aic_eval
+distrobox enter -r aic_eval
+
+# Inside the container, start the environment
+/entrypoint.sh ground_truth:=false start_aic_engine:=true
